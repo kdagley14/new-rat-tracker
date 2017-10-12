e16b975e8f3b472e6742a9a179ca762789195e32
Paige Rutherford <paigerutherford8@gmail.com>
2017-10-11 20:49:33 -0400

    added detailrequest

---
+++ app/src/main/java/team10x/cs2340/rattracker2/DetailRequest.java
@@ -0,0 +1,26 @@
+package team10x.cs2340.rattracker2;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DetailRequest extends StringRequest {
+
+    private static final String DETAIL_REQUEST_URL = "http://rat-tracker.000webhostapp.com/Detail.php";
+    private Map<String, String> params;
+
+    // access the database and send the primary key to get the rest of the data
+    public DetailRequest(String primaryId, Response.Listener<String> listener) {
+        super(Request.Method.POST, DETAIL_REQUEST_URL, listener, null);
+        params = new HashMap<>();
+        params.put("key", primaryId);
+    }
+
+    @Override
+    public Map<String, String> getParams() {
+        return params;
+    }
+}
