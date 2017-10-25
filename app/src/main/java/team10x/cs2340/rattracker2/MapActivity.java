4c9a053d823a231ea2996a18b84e29f01e92f886
Kristin Dagley <kdagley14@gmail.com>
2017-10-24 23:08:34 -0400

    Making the map actually work

--- app/src/main/java/team10x/cs2340/rattracker2/MapActivity.java
+++ app/src/main/java/team10x/cs2340/rattracker2/MapActivity.java
@@ -2,23 +2,31 @@ package team10x.cs2340.rattracker2;
 
 import android.content.Intent;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
+    private SupportMapFragment sMapFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
+        sMapFragment = SupportMapFragment.newInstance();
 
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         mToggle = new ActionBarDrawerToggle(MapActivity.this, mDrawerLayout, R.string.open, R.string.closed);
@@ -51,11 +59,17 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
                 return true;
             }
         });
+
+        sMapFragment.getMapAsync(MapActivity.this);
+        FragmentManager sFm = getSupportFragmentManager();
+        sFm.beginTransaction().add(R.id.map, sMapFragment).commit();
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-
+        LatLng newYork = new LatLng(40, -73);
+        googleMap.addMarker(new MarkerOptions().position(newYork).title("HEY THERE"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(newYork));
     }
 
     @Override
