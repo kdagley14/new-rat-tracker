75f833176a557238680e517cac0f2bd6b7cd761f
Kristin Dagley <kdagley14@gmail.com>
2017-11-08 00:38:38 -0500

    making the navigation drawer show up on the GraphDateRangeActivity

--- app/src/main/java/team10x/cs2340/rattracker2/GraphDateRangeActivity.java
+++ app/src/main/java/team10x/cs2340/rattracker2/GraphDateRangeActivity.java
@@ -1,22 +1,44 @@
 package team10x.cs2340.rattracker2;
 
 import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class GraphDateRangeActivity extends AppCompatActivity {
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_search_date_range);
+        setContentView(R.layout.activity_graph_date_range);
 
         final EditText etStartDate = (EditText) findViewById(R.id.etStartDate);
         final EditText etEndDate = (EditText) findViewById(R.id.etEndDate);
         final Button bViewGraph = (Button) findViewById(R.id.view_graph_button);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mToggle = new ActionBarDrawerToggle(GraphDateRangeActivity.this, mDrawerLayout, R.string.open, R.string.closed);
+
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        NavigationView nv = (NavigationView)findViewById(R.id.navigation_view);
+        nv.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                NavigationDrawer.getActivityOptions(GraphDateRangeActivity.this, menuItem);
+                return true;
+            }
+        });
 
         bViewGraph.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,5 +52,13 @@ public class GraphDateRangeActivity extends AppCompatActivity {
                 GraphDateRangeActivity.this.startActivity(viewGraphIntent);
             }
         });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(mToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
