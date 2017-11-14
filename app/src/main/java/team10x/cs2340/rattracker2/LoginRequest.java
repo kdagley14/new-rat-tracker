46ea0f565fad3063bc4991c858914dee38f910cc
kts7 <31508920+kts7@users.noreply.github.com>
2017-11-13 22:04:00 -0500

    javadocs

--- app/src/main/java/team10x/cs2340/rattracker2/LoginRequest.java
+++ app/src/main/java/team10x/cs2340/rattracker2/LoginRequest.java
@@ -17,7 +17,14 @@ public class LoginRequest extends StringRequest {
     private static final String LOGIN_REQUEST_URL = "http://rat-tracker.000webhostapp.com/Login.php";
     private Map<String, String> params;
 
-    // access the database and check to see if the credentials are in the database
+    /**
+    * Method creates functionality for user login function
+    *
+    * @param username  text placed in field created for username input
+    * @param password  text placed in field created for password input
+    * @param listener  response listener to read the user's input
+    * @return params  information given by method parameters
+    */
     public LoginRequest(String username, String password, Response.Listener<String> listener) {
         super(Request.Method.POST, LOGIN_REQUEST_URL, listener, null);
         params = new HashMap<>();
@@ -26,6 +33,10 @@ public class LoginRequest extends StringRequest {
 
     }
 
+    /**
+    * Method returns parameters as map object
+    * @return params  map object created by previously given information
+    */
     @Override
     public Map<String, String> getParams() {
         return params;
