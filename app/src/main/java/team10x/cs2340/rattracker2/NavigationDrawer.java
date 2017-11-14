585fa0cb93956b54947c1c66c9f848aafa38200f
kts7 <31508920+kts7@users.noreply.github.com>
2017-11-13 22:42:39 -0500

    javadoc

--- app/src/main/java/team10x/cs2340/rattracker2/NavigationDrawer.java
+++ app/src/main/java/team10x/cs2340/rattracker2/NavigationDrawer.java
@@ -5,36 +5,42 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 
 /**
- * Created by kdagley on 11/1/17.
- *
- * This class creates the navigation menu
- * found in the top corner of the application
- * along with each individual menu item
- */
+* Created by kdagley on 11/1/17.
+*
+* This class creates the navigation menu
+* found in the top corner of the application
+* along with each individual menu item
+*/
+public class NavigationDrawer {
 
-class NavigationDrawer {
-
+    /**
+    * Method sets each available activity within the application
+    * to a seperate menu item
+    * @param currActivity  current activity state of application
+    * @param menuItem  new menu item object 
+    */
     public static void getActivityOptions(AppCompatActivity currActivity, MenuItem menuItem) {
+        final AppCompatActivity current = currActivity;
         switch (menuItem.getItemId()) {
             case (R.id.reports_list):
-                Intent reportsListIntent = new Intent(currActivity, HomeActivity.class);
-                currActivity.startActivity(reportsListIntent);
+                Intent reportsListIntent = new Intent(current, HomeActivity.class);
+                current.startActivity(reportsListIntent);
                 break;
             case (R.id.create_report):
-                Intent createReportIntent = new Intent(currActivity, CreateReportActivity.class);
-                currActivity.startActivity(createReportIntent);
+                Intent createReportIntent = new Intent(current, CreateReportActivity.class);
+                current.startActivity(createReportIntent);
                 break;
             case (R.id.view_map):
-                Intent viewMapIntent = new Intent(currActivity, MapActivity.class);
-                currActivity.startActivity(viewMapIntent);
+                Intent viewMapIntent = new Intent(current, MapActivity.class);
+                current.startActivity(viewMapIntent);
                 break;
             case (R.id.view_graphs):
-                Intent viewGraphsIntent = new Intent(currActivity, GraphDateRangeActivity.class);
-                currActivity.startActivity(viewGraphsIntent);
+                Intent viewGraphsIntent = new Intent(current, GraphDateRangeActivity.class);
+                current.startActivity(viewGraphsIntent);
                 break;
             case (R.id.logout):
-                Intent logoutIntent = new Intent(currActivity, MainActivity.class);
-                currActivity.startActivity(logoutIntent);
+                Intent logoutIntent = new Intent(current, MainActivity.class);
+                current.startActivity(logoutIntent);
         }
     }
 }
